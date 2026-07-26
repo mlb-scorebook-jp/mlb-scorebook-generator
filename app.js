b8bd7896b3d6276409dd8d4fb516d7c56d741e4e
mlb-scorebook-jp <hiramot.akihiro+mlb@gmail.com>
2026-07-26 10:34:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -94,6 +94,7 @@ console.log(gameData);
 console.log(gameData.gameData);
 console.log(gameData.gameData.teams);
 console.log(gameData.gameData.teams.home.name);
+
     setGameInfo(0, "HOME", "取得準備中");
     setGameInfo(1, "AWAY", "取得準備中");
     setGameInfo(2, "DATE", "取得準備中");
